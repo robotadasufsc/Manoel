cc71d41c7c732e21da2a58177ae3e06ba11825a9
FelippeRoza <felippesroza@gmail.com>
2017-10-31 18:52:21 -0200

    multiple changes

--- Manoel.ino
+++ Manoel.ino
@@ -1,8 +1,15 @@
-#include "submodules/Exposer/exposer.h"
-#include "submodules/Exposer/exposer.cpp"
+//#include "submodules/Exposer/exposer.h"
+//#include "submodules/Exposer/exposer.cpp"
 #include <Servo.h>
 
-Exposer* exposer = &Exposer::self();
+#define LOOP_DELAY 2000
+#define RIGHT_B 1600
+#define LEFT_B 1600
+#define RIGHT_F 1200
+#define LEFT_F 1200
+#define STOP_MOTOR 1500
+
+//Exposer* exposer = &Exposer::self();
 
 uint8_t led = 0;
 
@@ -16,6 +23,9 @@ float testfloat = 0;
 String teststring = "Batata";
 
 unsigned long next;
+
+char rc;
+int robot_vel;
 
 Servo left_wheel;
 Servo right_wheel;
@@ -36,24 +46,74 @@ void setup()
     exposer->registerVariable(VARNAME(testint32), Exposer::_int32_t, &testint32);
     exposer->registerVariable(VARNAME(testfloat), Exposer::_float, &testfloat);
     exposer->registerVariable(VARNAME(teststring), Exposer::_string, &teststring);
-  */
+
     exposer->registerVariable(VARNAME(left_pwm), Exposer::_int16_t, &left_pwm);
     exposer->registerVariable(VARNAME(right_pwm), Exposer::_int16_t, &right_pwm);
-    next = millis()+1000;
+    next = millis()+1000;  */
+    delay(50);
 }
 
 void loop()
 {
 
-
-
-    exposer->update();  
+  while(!Serial.available() ){}
+    rc = Serial.read();
+    Serial.println(rc);
+    if(rc == 'm'){
+      while(!Serial.available() ){}
+      rc = Serial.read();
+      move_robot(rc);
+    }
+    if(rc == 'c'){
+      Serial.println("Pegar copo");
+    }
+    if(rc == 'o'){
+      Serial.println("Ordenhar vaca");
+    }    
+    if(rc == 'q'){
+      while(!Serial.available() ){}
+      robot_vel = Serial.parseInt();
+      Serial.println(robot_vel);
+      left_wheel.writeMicroseconds(robot_vel);
+      right_wheel.writeMicroseconds(robot_vel);
+      delay(10);
+      left_wheel.writeMicroseconds(robot_vel);
+      right_wheel.writeMicroseconds(robot_vel);
+    }
+    /*exposer->update();  
 
     if (millis()>next){
         left_wheel.writeMicroseconds(left_pwm);
         right_wheel.writeMicroseconds(right_pwm);
         next = millis()+1000;
-    }
+    }*/
 
 } 
+
+void move_robot(char dir){
+  left_wheel.writeMicroseconds(1400);
+  right_wheel.writeMicroseconds(1400);
+  delay(LOOP_DELAY*2);
+  if(dir == 'f'){
+    left_wheel.writeMicroseconds(LEFT_F);
+    right_wheel.writeMicroseconds(RIGHT_F);
+  }
+  if(dir == 'b'){
+    left_wheel.writeMicroseconds(LEFT_B);
+    right_wheel.writeMicroseconds(RIGHT_B);
+  }
+  if(dir == 'l'){
+    left_wheel.writeMicroseconds(LEFT_B);
+    right_wheel.writeMicroseconds(RIGHT_F);
+  }
+  if(dir == 'r'){
+    left_wheel.writeMicroseconds(LEFT_F);
+    right_wheel.writeMicroseconds(RIGHT_B);
+  }
+
+  delay(LOOP_DELAY);
+  left_wheel.writeMicroseconds(STOP_MOTOR);
+  right_wheel.writeMicroseconds(STOP_MOTOR);
+  delay(50);
+}
 
